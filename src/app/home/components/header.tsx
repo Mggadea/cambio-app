1b0ef05727fd7e62c51430298df4b76964f98e61
mggadea <matiasgaraygadea@outlook.com>
2026-04-26 20:36:29 -0300

    styles: styles to match design

--- src/app/home/components/header.tsx
+++ src/app/home/components/header.tsx
@@ -5,8 +5,8 @@ interface HeaderProps {
 function Header({ title }: HeaderProps) {
   return (
     <header className="bg-indigo-950 px-6 py-6">
-      <div className="mx-auto w-full max-w-6xl">
-        <p className="text-3xl font-bold text-slate-100">{title}</p>
+      <div className="px-12 w-full max-w-6xl">
+        <p className="text-2xl font-bold text-slate-100">{title}</p>
       </div>
     </header>
   );
